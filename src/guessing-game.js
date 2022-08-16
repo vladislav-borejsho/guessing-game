a21d9c58014b0e90621e8e64019ab2ab53864795
vladislav.borejsho <bjiadisjiav@gmail.com>
2022-08-16 20:15:32 +0300

    task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.mid
     }
 
     guess() {
-
+        return Math.round((this.max-this.min) / 2) + this.min;
     }
 
     lower() {
-
+        return this.max = (Math.round((this.max-this.min) / 2) + this.min);
     }
 
     greater() {
-
+        return this.min = (Math.round((this.max-this.min) / 2) + this.min);
     }
 }
 
